d15a40bd897a2558e0e70033d0b5965dbe488e19
WiebeHero <50573980+WiebeHero@users.noreply.github.com>
2020-01-04 01:47:33 +0100

    ok
    ok

--- src/me/WiebeHero/Scoreboard/DFScoreboard.java
+++ src/me/WiebeHero/Scoreboard/DFScoreboard.java
@@ -59,6 +59,125 @@ public class DFScoreboard implements Listener{
 			delay.put(player.getUniqueId(), player.getChunk());
 		}
 	}
+	public void hardRefreshScoreboards(Player player) {
+		scoreboards.put(player.getUniqueId(), null);
+		Scoreboard scoreboard = CustomEnchantments.getInstance().getServer().getScoreboardManager().getNewScoreboard();
+		//--------------------------------------------------------------------------------------
+		//Set health scoreboard
+		//--------------------------------------------------------------------------------------
+		Objective o = scoreboard.registerNewObjective("health", "health", "display");
+		o.setDisplayName(new CCT().colorize("&c❤"));
+		o.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		//--------------------------------------------------------------------------------------
+		//Set normal scoreboard
+		//--------------------------------------------------------------------------------------
+		Objective score = scoreboard.registerNewObjective("score", "", "");
+		score.setDisplayName(new CCT().colorize("&2&lDungeonForge"));
+		score.setDisplaySlot(DisplaySlot.SIDEBAR);
+		DFFaction faction = method.getFaction(player.getUniqueId());
+		//--------------------------------------------------------------------------------------
+		//Blank Scores
+		//--------------------------------------------------------------------------------------
+		Score blank1 = score.getScore("");
+		Score blank2 = score.getScore(" ");
+		Score blank3 = score.getScore("  ");
+		//--------------------------------------------------------------------------------------
+		//Faction setting
+		//--------------------------------------------------------------------------------------
+		String facN = "&7None";
+		String territory = "";
+		if(faction != null) {
+			facN = "&6" + faction.getName();
+			if(faction.isInChunk(player)) {
+				territory = "&a&l" + faction.getName();
+			}
+		}
+		else if(method.isInAChunk(player)) {
+			for(DFFaction fac : CustomEnchantments.getInstance().factionList) {
+				if(fac.getChunkList().contains(player.getLocation().getChunk())) {
+					territory = "&c&l" + fac.getName();
+				}
+			}
+		}
+		else {
+			territory = "&7Wilderness";
+		}
+		Score facLine = score.getScore(new CCT().colorize("&7Faction: " + facN));
+		Score facTeritory = score.getScore(new CCT().colorize("&7Territory: " + territory));
+		Score rank = score.getScore(new CCT().colorize("&7Rank: " + ranks.get(player.getUniqueId())));
+		Score level = score.getScore(new CCT().colorize("&7Level: &b&l" + def.getPlayer(player).getLevel()));
+		Score adress = score.getScore(new CCT().colorize("    &2&lplay.dungeonforge.net"));
+		Score cash = score.getScore(new CCT().colorize("&7Money: &a$" + String.format("%.2f", money.getMoneyList().get(player.getUniqueId()))));
+		for(Player p : Bukkit.getOnlinePlayers()) {
+			DFPlayer dfPlayer = new DFPlayer().getPlayer(p);
+			Team t = null;
+			if(scoreboard.getTeam(p.getName()) == null) {
+				t = scoreboard.registerNewTeam(p.getName());
+			}
+			else {
+				t = scoreboard.getTeam(p.getName());
+			}
+			t.setPrefix(new CCT().colorize("&6[&b" + dfPlayer.getLevel() + "&6]&7 "));
+			String stringClass = dfPlayer.getPlayerClass().toString().toLowerCase();
+			String now = stringClass.substring(0, 1).toUpperCase() + stringClass.substring(1);
+			t.setSuffix(new CCT().colorize(" &6" + now));
+			p.setPlayerListName(new CCT().colorize(t.getPrefix() + p.getName() + " " + ranks.get(p.getUniqueId())));
+			t.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.ALWAYS);
+			t.addEntry(p.getName());
+		}
+		//--------------------------------------------------------------------------------------
+		//Adding me to others scoreboard
+		//--------------------------------------------------------------------------------------
+		for(UUID uuid : scoreboards.keySet()) {
+			Player p = Bukkit.getPlayer(uuid);
+			if(p != null) {
+				Scoreboard other = p.getScoreboard();
+				if(other != null) {
+					if(other.getTeam(p.getName()) != null) {
+						if(!other.getTeam(p.getName()).hasEntry(player.getName())) {
+							other.getTeam(p.getName()).addEntry(player.getName());
+						}
+					}
+				}
+			}
+		}
+		if(scoreboard.getTeam("GRAY") == null) {
+			scoreboard.registerNewTeam("GRAY");
+			scoreboard.getTeam("GRAY").setPrefix(ChatColor.GRAY + "");
+			scoreboard.getTeam("GRAY").setColor(ChatColor.GRAY);
+			scoreboard.registerNewTeam("GREEN");
+			scoreboard.getTeam("GREEN").setPrefix(ChatColor.GREEN + "");
+			scoreboard.getTeam("GREEN").setColor(ChatColor.GREEN);
+			scoreboard.registerNewTeam("AQUA");
+			scoreboard.getTeam("AQUA").setPrefix(ChatColor.AQUA + "");
+			scoreboard.getTeam("AQUA").setColor(ChatColor.AQUA);
+			scoreboard.registerNewTeam("RED");
+			scoreboard.getTeam("RED").setPrefix(ChatColor.RED + "");
+			scoreboard.getTeam("RED").setColor(ChatColor.RED);
+			scoreboard.registerNewTeam("PURPLE");
+			scoreboard.getTeam("PURPLE").setPrefix(ChatColor.DARK_PURPLE + "");
+			scoreboard.getTeam("PURPLE").setColor(ChatColor.DARK_PURPLE);
+			scoreboard.registerNewTeam("YELLOW");
+			scoreboard.getTeam("YELLOW").setPrefix(ChatColor.YELLOW + "");
+			scoreboard.getTeam("YELLOW").setColor(ChatColor.YELLOW);
+		}
+		Set<String> entries;
+        entries = scoreboard.getEntries();
+        for(String entry : entries){
+        	scoreboard.resetScores(entry);
+        }
+        blank1.setScore(9);
+		facLine.setScore(8);
+		facTeritory.setScore(7);
+		cash.setScore(6);
+		blank2.setScore(5);
+		rank.setScore(4);
+		level.setScore(3);
+		blank3.setScore(2);
+		adress.setScore(1);
+		scoreboards.put(player.getUniqueId(), scoreboard);
+		player.setScoreboard(scoreboard);
+	}
 	public void generateScoreboard(Player player) {
 		scoreboards.put(player.getUniqueId(), null);
 		Scoreboard scoreboard = CustomEnchantments.getInstance().getServer().getScoreboardManager().getNewScoreboard();
@@ -108,29 +227,22 @@ public class DFScoreboard implements Listener{
 		Score level = score.getScore(new CCT().colorize("&7Level: &b&l" + def.getPlayer(player).getLevel()));
 		Score adress = score.getScore(new CCT().colorize("    &2&lplay.dungeonforge.net"));
 		Score cash = score.getScore(new CCT().colorize("&7Money: &a$" + String.format("%.2f", money.getMoneyList().get(player.getUniqueId()))));
-		//--------------------------------------------------------------------------------------
-		//Prefix/Suffix setting
-		//--------------------------------------------------------------------------------------
-		DFPlayer dfPlayer = new DFPlayer().getPlayer(player);
-		Team t = scoreboard.registerNewTeam(player.getName());
-		teams.put(player.getUniqueId(), t);
-		t.setPrefix(new CCT().colorize("&6[&b" + dfPlayer.getLevel() + "&6]&7 "));
-		String stringClass = dfPlayer.getPlayerClass().toString().toLowerCase();
-		String now = stringClass.substring(0, 1).toUpperCase() + stringClass.substring(1);
-		t.setSuffix(new CCT().colorize(" &6" + now));
-		player.setPlayerListName(new CCT().colorize(t.getPrefix() + player.getName() + " " + ranks.get(player.getUniqueId())));
-		t.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.ALWAYS);
-		t.addEntry(player.getName());
-		//--------------------------------------------------------------------------------------
-		//Adding players to my scoreboard
-		//--------------------------------------------------------------------------------------
-		for(UUID uuid : scoreboards.keySet()) {
-			Player p = Bukkit.getPlayer(uuid);
-			if(p != null) {
-				if(!scoreboard.getTeam(player.getName()).hasEntry(p.getName())) {
-					t.addEntry(p.getName());
-				}
+		for(Player p : Bukkit.getOnlinePlayers()) {
+			DFPlayer dfPlayer = new DFPlayer().getPlayer(p);
+			Team t = null;
+			if(scoreboard.getTeam(p.getName()) == null) {
+				t = scoreboard.registerNewTeam(p.getName());
 			}
+			else {
+				t = scoreboard.getTeam(p.getName());
+			}
+			t.setPrefix(new CCT().colorize("&6[&b" + dfPlayer.getLevel() + "&6]&7 "));
+			String stringClass = dfPlayer.getPlayerClass().toString().toLowerCase();
+			String now = stringClass.substring(0, 1).toUpperCase() + stringClass.substring(1);
+			t.setSuffix(new CCT().colorize(" &6" + now));
+			p.setPlayerListName(new CCT().colorize(t.getPrefix() + p.getName() + " " + ranks.get(p.getUniqueId())));
+			t.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.ALWAYS);
+			t.addEntry(p.getName());
 		}
 		//--------------------------------------------------------------------------------------
 		//Adding me to others scoreboard
